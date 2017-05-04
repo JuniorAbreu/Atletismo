f248d10aa6321e91c32ad6c6e86e1724bbb8db69
lapinfo <lapinfo@br.ibm.com>
2017-05-04 16:51:51 -0300

    test

--- src/main/java/br/com/fatec/atletismo/dao/GenericDAO.java
+++ src/main/java/br/com/fatec/atletismo/dao/GenericDAO.java
@@ -19,6 +19,8 @@ this.classe=	(Class<Entidades>)((ParameterizedType) getClass().getGenericSupercl
 }
 
 	
+	
+	
 	public void salvar(Entidades entidade){
 		
 		Connection con = JDBC.getInstance().getConnection();
